5ddcf34adf4de9eae6ab3a3b5c56199e90b823f8
Alistair A. Israel <aisrael@gmail.com>
2019-11-19 12:01:38 +0800

    hello.tsx -> greeter.tsx

--- apps/hello_react_web/assets/js/app.js
+++ apps/hello_react_web/assets/js/app.js
@@ -19,7 +19,7 @@ import "phoenix_html";
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Greeter from "./hello";
+import Greeter from "./greeter";
 
 const greeting = document.getElementById("greeting");
 ReactDOM.render(<Greeter name="Phoenix" />, greeting);
