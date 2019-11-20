4a67b71c8a4a64482d32b2658d762fc6c0696327
Alistair A. Israel <aisrael@gmail.com>
2019-11-20 22:00:31 +0800

    Fixing app.js
    (cherry picked from commit 3acde04c6358b0f87f2e1d4ab19d6de3319e5568)

--- apps/hello_react_web/assets/js/app.js
+++ apps/hello_react_web/assets/js/app.js
@@ -1,7 +1,7 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import _css from "../css/app.css";
+const _css = require("../css/app.css");
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
