c476982f65bf91edce704be695748d0e97f1b1b3
Alistair Israel <aisrael@gmail.com>
2021-12-31 16:52:25 -0500

    Greeter React component

--- apps/hello_web/assets/js/greeter.tsx
+++ apps/hello_web/assets/js/greeter.tsx
@@ -1,5 +1,17 @@
-function greet(name: string): string {
-    return "Welcome to " + name + " with TypeScript!";
+import React from "react";
+
+interface GreeterProps {
+    name: string;
 }
 
-export default greet;
+const Greeter: React.FC<GreeterProps> = (props: GreeterProps) => {
+    const name = props.name;
+    return (
+        <section className="phx-hero">
+            <h1>Welcome to {name} with TypeScript and React!</h1>
+            <p>Peace-of-mind from prototype to production</p>
+        </section>
+    );
+};
+
+export default Greeter;
